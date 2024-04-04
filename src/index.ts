f3ea71eebd7556a5e1a03bfd62efe3c4f5a9c1d3
Exel Tarkus <exel.tarkus@adira.co.id>
2024-04-04 10:24:58 +0700

    change port default

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import app from './app';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.APP_PORT || 8095;
+const port = process.env.APP_PORT || 3001;
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`Starting at http://localhost:${port}`);
 });
 
 export default app;
